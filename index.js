34dc1be79e15a1095b537b61ee4678f2f5429866
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-27 19:42:27 -0500

    Adds svg building functionality, and writes the data to logo.svg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const { Circle, Triangle, Square } = require("./lib/shapes");
+const fs = require("fs");
 
 const questions = [
 {
@@ -28,4 +29,16 @@ const questions = [
 inquirer.prompt(questions)
   .then(responses => {
     console.log(responses)
+    const { letters, color, shape, bgColor } = responses;
+    if (shape === "Circle") {
+      const newSVG = new Circle(color, letters, bgColor);
+    } else if (shape === "Triangle") {
+      const newSVG = new Triangle(color, letters, bgColor);
+    } else if (shape === "Square") {
+      const newSVG = new Square(color, letters, bgColor);
+    }
+
+    fs.writeFile("logo.svg", newSVG.build(), (err) => {
+      if (err) console.log("There was an error")
+    })
   })
